5674d4e26132a0bb5f805abab91e347232bbe5d9
SamDwis <140613647+SamuelDwiS@users.noreply.github.com>
2025-11-20 12:15:53 +0700

    Update route admin login

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ Route::get('/about', function () {
     return view('layouts/about');
 });
 
-// Route::get('login', [AdminController::class, 'login'])->name('login');
-// Route::post('login', [AdminController::class, 'login_action'])->name('login_action');
-// Route::post('password', [AdminController::class, 'password_action'])->name('password_action');
-// Route::get('logout', [AdminController::class, 'logout'])->name('logout');
+Route::get('/adm/login', function () {
+    return view('admin/login');
+});
+
+Route::get('login', [AdminController::class, 'login'])->name('login');
+Route::post('login', [AdminController::class, 'login_action'])->name('login.action');
+Route::post('password', [AdminController::class, 'password_action'])->name('password.action');
+Route::get('logout', [AdminController::class, 'logout'])->name('logout');
