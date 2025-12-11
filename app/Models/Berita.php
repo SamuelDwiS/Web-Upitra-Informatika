897c46cded5dc6b3536b1a508ee8977a7b0b5db0
SamDwis <140613647+SamuelDwiS@users.noreply.github.com>
2025-12-11 14:42:58 +0700

    Update Model Berita

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -7,12 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Berita extends Model
 {
 
-    protected $table = "berita";
+    protected $table = "tb_berita";
     protected $primaryKey = 'id_berita';
 
     protected $fillable = [
         'judul',
         'deskripsi',
+        'tgl_berita',
+        'kategori',
         'gambar',
     ];
 }
